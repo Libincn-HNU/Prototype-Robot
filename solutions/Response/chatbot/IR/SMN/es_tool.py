c0433c743515bbafb7745fe925e206899810eee0
Apollo2Mars <Apollo2Mars@gmail.com>
2020-02-11 10:51:39 +0800

    update

--- solutions/Response/chatbot/IR/SMN/es_tool.py
+++ solutions/Response/chatbot/IR/SMN/es_tool.py
@@ -4,13 +4,18 @@ from elasticsearch import RequestsHttpConnection, Elasticsearch
 from elasticsearch.helpers import bulk
 
 
-all_index_name = 'chatbot_corpus_60'
+all_index_name = 'chatbot_corpus_62'
+all_index_type = 'doc'
+all_file_name = 'corpus-step-1.pkl'
 
 class Build(object):
     def __init__(self, ip='127.0.0.1'):
+        self.index_name = all_index_name
+        self.index_type = all_index_type
+        self.file_name = all_file_name
         self.es = Elasticsearch([ip], port=9200)
 
-    def create_index(self, index_name, file_name, index_type='ott_date'):
+    def create_index(self):
         _index_mappings = {
             "mappings":{
                 "properties":{
@@ -24,45 +29,47 @@ class Build(object):
                 }
             }
         
-        if self.es.indices.exists(index=index_name) is not True: # index 不存在 则创建
-            res = self.es.indices.create(index=index_name, body=_index_mappings)
+        if self.es.indices.exists(index=self.index_name) is not True: # index 不存在 则创建
+            res = self.es.indices.create(index=self.index_name, body=_index_mappings)
             print(res)
 
-            with open(file_name, 'rb') as f:
-                results = pickle.load(f)
+    def build(self):
+        with open(self.file_name, 'rb') as f:
+            results = pickle.load(f)
 
-                i = 1
-                ACTIONS = []
+            i = 1
+            ACTIONS = []
 
-                for item in results:
-                    idx = 0
-                    while idx < len(item) - 1:
-                        action  = {
-                            '_index':index_name,
-                            '_type':index_type,
-                            '_id':i, 
-                            '_source':{
-                                'query':item[idx],
-                                'answer':item[idx+1]                
-                            }
+            for item in results:
+                idx = 0
+                while idx < len(item) - 1:
+                    action  = {
+                        '_index':self.index_name,
+                        '_type':self.index_type,
+                        '_id':i, 
+                        '_source':{
+                        'query':item[idx],
+                        'answer':item[idx+1]                
                         }
-                        idx +=1 
-                        i += 1
-                        ACTIONS.append(action)
+                    }
+                    idx +=1 
+                    i += 1
+                    ACTIONS.append(action)
 
-                        if i % 100000 == 0:
-                            print('index is ', i)
-                            print(action)
+                    if i % 1000000 == 0:
+                        print('index is ', i)
+                        print(action)
 
-                success, _ = bulk(self.es, ACTIONS, index=index_name, raise_on_error=True)
-                print("Performed %d actions " % success)
+            success, _ = bulk(self.es, ACTIONS, index=self.index_name, raise_on_error=True)
+            print("Performed %d actions " % success)
+
 
 class Search(object):
     
     def __init__(self, ip='127.0.0.1'):
         self.es = Elasticsearch([ip], port=9200)
 
-    def create_index(self, index_name=all_index_name, index_type='ott_date'):
+    def create_index(self, index_name=all_index_name, index_type=all_index_type):
         """
         创建索引
         """
@@ -91,17 +98,17 @@ class Operate(object):
 
 
     def search_query(self):
-        obj = ElasticObj('qa_info', 'qa_detail')
+        obj = ElasticObj(all_index_name, all_index_type)
         answer_list = obj.Get_Data_By_Body(sys.argv[1])
         print(answer_list)
 
     def search_query_and_delete(self):
-        obj = ElasticObj('qa_info', 'qa_detail')
+        obj = ElasticObj(all_index_name, all_index_type)
         answer_list = obj.Get_Data_By_Body(sys.argv[1])
         obj.Delete_Data_By_Body(sys.argv[1])
     
     def search_answer_and_delete(self):
-        obj = ElasticObj('qa_info', 'qa_detail')
+        obj = ElasticObj(all_index_name, all_index_type)
         answer_list = obj.Get_Data_By_Answer(sys.argv[1])
         obj.Delete_Data_By_Body(sys.argv[1])
 
@@ -146,9 +153,10 @@ class ElasticObj(object):
         print(result)
 
 
-def build(index_name = all_index_name, file_name='corpus-step-1.pkl'):
+def build():
     build = Build()
-    build.create_index(index_name=index_name, index_type='qa_detail', file_name= file_name)
+    build.create_index()
+    build.build()
 
 def search():
     ope = Operate()
@@ -158,5 +166,5 @@ def search():
 if __name__ == '__main__':
     build()
 
-    search()
+   #  search()
     
