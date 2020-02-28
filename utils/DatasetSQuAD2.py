611f65da3c7d5bcebe18dff4bf587076dc3c47f4
apollo2mars <apollo2mars@gmail.com>
2020-02-28 11:41:16 +0800

    update utils

---
+++ utils/DatasetSQuAD2.py
@@ -0,0 +1,27 @@
+# coding:utf-8
+# author:Apollo2Mars@gmail.com
+# function:dataset loader for squad2
+
+import tensorflow as tf
+
+class DatasetSQuAD2():
+	def __init__(self, opt, tokenizer):
+		self.opt = opt
+
+		self.tokenizer = tokenizer
+
+		self.max_query_len = opt.max_query_len
+		self.max_passage_len = opt.max_passage_len
+		self.max_passage_num = opt.max_passage_num
+
+
+
+
+
+	def preprocess(self):
+		train_data_loader = tf.data.Dataset.from_tensor_slices({'text': self.trainset.text_list}).batch(
+			self.opt.batch_size).shuffle(10000)
+		test_data_loader = tf.data.Dataset.from_tensor_slices({'text': self.testset.text_list}).batch(
+			self.opt.batch_size)
+		dev_data_loader = test_data_loader
+
